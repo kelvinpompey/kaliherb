6b7b284a8867ed1000c1908635462e95f24cd393
Kelvin Pompey <kelvin.pompey@gmail.com>
2015-04-23 12:42:47 -0400

    render products by Category

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,16 +4,23 @@ var ProductCatalog = React.createClass({
 		var Table = ReactBootstrap.Table; 
 		var ButtonGroup = ReactBootstrap.ButtonGroup; 
 		var Button = ReactBootstrap.Button; 
-		var tableRows = this.props.products.map(function(product, index){
-			return (
-				<tr key={index}>
-					<td><img src={product.image} width={100} /></td>
-					<td>{product.category}</td>
-					<td>{product.title}</td>
-					<td>{product.description}</td>
-				</tr>
-			); 
-		}); 
+
+		var keys = []; 
+		var comps = {}; 
+
+		for(key in this.props.products) {
+			keys.push(key); 
+			comps[key] = this.props.products[key].map(function(product, index){
+				return (				
+					<tr key={index}>
+						<td><img src={product.image} width={100} /></td>
+						<td>{product.title}</td>
+						<td>{product.description}</td>
+					</tr>
+				); 
+			}); 
+		}
+
 		return (
 			<div className="container">
 				<h1>Product Catalog</h1>
@@ -26,13 +33,16 @@ var ProductCatalog = React.createClass({
 						<thead>
 							<tr>
 								<th>Image</th>
-								<th>Category</th>
 								<th width={200}>Title</th>
 								<th>Description</th>
 							</tr>
 						</thead>
-						<tbody>
-							{tableRows}					
+						<tbody>							
+							{keys.map(function(key){
+								return (
+									[<tr><td colSpan={3}>{key}</td></tr>].concat(comps[key])
+								);
+							})}					
 						</tbody>
 					</Table>
 				</div>
@@ -47,11 +57,21 @@ var ProductCatalog = React.createClass({
 
 
 //var ProductGrid = React.createClass({}); 
-var products = [
+var indica = [
 	{category: 'Indica', title: 'Jedi OG', key: 1, image: 'https://d2kxqxnk1i5o9a.cloudfront.net/uploads/pictures/menu_items/844291/large_Jedi_.jpg', description: 'This indica produces strong body high with minimal effect on perception; it’s great for chronic pain relief. A High Times bud of the year winner. A cross between Deathstar and San Fernando Valley OG Kush that blends into a great marijuana strain.'}, 
 	{category: 'Indica', title: 'Purple Chem Dawg', image: 'https://d2kxqxnk1i5o9a.cloudfront.net/uploads/pictures/menu_items/819311/large_P.CHEM_DAWG_.jpeg', key: 2, description: 'It has a slight numbing analgesic effect on the sinuses, lungs and throat similar to the feeling of cold medications. The offspring of Grandaddy Purps and Chem Dog, this hybrid encourages introspective thoughts and reasoning. It is good for anxiety, upper body pain, migraines, depression, PTSD, PMS and nausea.'}, 
 	{category: 'Indica', title: 'Grand Daddy Diesel', image: 'https://d2kxqxnk1i5o9a.cloudfront.net/uploads/pictures/menu_items/999515/large_Cherry_Pie_Cannabis_strain_1.PNG', key: 3, description: 'N/A'}, 
 ]; 
+
+var sativa = [
+	{category: 'Sativa', title: 'CANNA- TSU HIGH CBD', key: 1, image: 'https://d2kxqxnk1i5o9a.cloudfront.net/uploads/pictures/menu_items/1113998/large_320Canna-tsu.jpg', description: 'Harlequin is a 75/25 sativa-dominant strain renowned for its reliable expression of CBD. you can depend on Harlequin to provide clear-headed, alert sativa effects. this strain one of the most effective out there for treatment of pain and anxiety, as CBD counteracts THC’s paranoia while amplifying its painkilling properties.'}, 
+	{category: 'Sativa', title: 'HARLEQUIN HIGH CBD', image: 'https://d2kxqxnk1i5o9a.cloudfront.net/uploads/pictures/menu_items/954661/large_harlequin_high_cbd_strain_at_elemental_wellness_medical_marijuana_collective_in_san_jose.jpeg', key: 2, description: 'Harlequin is a 75/25 sativa-dominant strain renowned for its reliable expression of CBD. you can depend on Harlequin to provide clear-headed, alert sativa effects. this strain one of the most effective out there for treatment of pain and anxiety, as CBD counteracts THC’s paranoia while amplifying its painkilling properties.'}	
+];
+
+
+var products = {}; 
+products['indica'] = indica; 
+products['sativa'] = sativa; 
 
 var KaliherbApp = React.createClass({
 	render: function() {
