c4a70d9a532ccc3842f13961574f1acd682e7ea8
Kelvin Pompey <kelvin.pompey@gmail.com>
2015-04-23 16:57:49 -0400

    capitalize category title

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -40,7 +40,7 @@ var ProductCatalog = React.createClass({
 						<tbody>							
 							{keys.map(function(key){
 								return (
-									[<tr><td colSpan={3}>{key}</td></tr>].concat(comps[key])
+									[<tr><td colSpan={3} style={{textTransform: "uppercase"}}>{key}</td></tr>].concat(comps[key])
 								);
 							})}					
 						</tbody>
