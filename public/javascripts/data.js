d7a8c14d31406f57bb07706e239f1ddbe0811008
Kelvin Pompey <kelvin.pompey@gmail.com>
2015-04-27 13:51:11 -0400

    put test data in data.js

---
+++ public/javascripts/data.js
@@ -0,0 +1,74 @@
+var indica = [
+	{category: 'Indica', 
+	 title: 'Jedi OG', 
+	 key: 1, 
+	 image: 'https://d2kxqxnk1i5o9a.cloudfront.net/uploads/pictures/menu_items/844291/large_Jedi_.jpg', 
+	 description: 'This indica produces strong body high with minimal effect on perception; it’s great for chronic pain relief. A High Times bud of the year winner. A cross between Deathstar and San Fernando Valley OG Kush that blends into a great marijuana strain.',
+	 prices: {
+	 	'gram': 10, 
+	 	'eighth' : 50, 
+	 	'quarter': 90, 
+	 	'half' : 150, 
+	 	'ounce': 270 
+	 }	 
+	}, 
+	{category: 'Indica', 
+	 title: 'Purple Chem Dawg', 
+	 image: 'https://d2kxqxnk1i5o9a.cloudfront.net/uploads/pictures/menu_items/819311/large_P.CHEM_DAWG_.jpeg', 
+	 key: 2, 
+	 description: 'It has a slight numbing analgesic effect on the sinuses, lungs and throat similar to the feeling of cold medications. The offspring of Grandaddy Purps and Chem Dog, this hybrid encourages introspective thoughts and reasoning. It is good for anxiety, upper body pain, migraines, depression, PTSD, PMS and nausea.',
+	 prices: {
+	 	'gram': 10, 
+	 	'eighth' : 50, 
+	 	'quarter': 90, 
+	 	'half' : 150, 
+	 	'ounce': 270 
+	 }	 
+	}, 
+	{category: 'Indica', 
+	 title: 'Grand Daddy Diesel', 
+	 image: 'https://d2kxqxnk1i5o9a.cloudfront.net/uploads/pictures/menu_items/999515/large_Cherry_Pie_Cannabis_strain_1.PNG', 
+	 key: 3, 
+	 description: 'N/A',
+	 prices: {
+	 	'gram': 10, 
+	 	'eighth' : 50, 
+	 	'quarter': 90, 
+	 	'half' : 150, 
+	 	'ounce': 270 
+	 }}, 
+]; 
+
+var sativa = [
+	{category: 'Sativa', 
+	 title: 'CANNA- TSU HIGH CBD', 
+	 key: 1, 
+	 image: 'https://d2kxqxnk1i5o9a.cloudfront.net/uploads/pictures/menu_items/1113998/large_320Canna-tsu.jpg', 
+	 description: 'Harlequin is a 75/25 sativa-dominant strain renowned for its reliable expression of CBD. you can depend on Harlequin to provide clear-headed, alert sativa effects. this strain one of the most effective out there for treatment of pain and anxiety, as CBD counteracts THC’s paranoia while amplifying its painkilling properties.',
+	 prices: {
+	 	'gram': 10, 
+	 	'eighth' : 50, 
+	 	'quarter': 90, 
+	 	'half' : 150, 
+	 	'ounce': 270 
+	 }	
+	}, 
+	{category: 'Sativa', 
+	title: 'HARLEQUIN HIGH CBD', 
+	image: 'https://d2kxqxnk1i5o9a.cloudfront.net/uploads/pictures/menu_items/954661/large_harlequin_high_cbd_strain_at_elemental_wellness_medical_marijuana_collective_in_san_jose.jpeg', 
+	key: 2, 
+	description: 'Harlequin is a 75/25 sativa-dominant strain renowned for its reliable expression of CBD. you can depend on Harlequin to provide clear-headed, alert sativa effects. this strain one of the most effective out there for treatment of pain and anxiety, as CBD counteracts THC’s paranoia while amplifying its painkilling properties.',
+	 prices: {
+	 	'gram': 10, 
+	 	'eighth' : 50, 
+	 	'quarter': 90, 
+	 	'half' : 150, 
+	 	'ounce': 270 
+	 }	
+	}	
+];
+
+
+var products = {}; 
+products['indica'] = indica; 
+products['sativa'] = sativa;
